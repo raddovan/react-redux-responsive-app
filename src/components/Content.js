6d0eb56940ca0ae4ae44f77c706c5f98073544ae
radovan <preradovic.radovan@gmail.com>
2022-03-04 15:08:29 +0100

    Content stlyes

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,16 @@
 import { Container, Row, Col } from 'react-grid-system';
 import ai from "../assets/ai.png";
+import Typography from '@mui/material/Typography';
+import "./styling.css";
 
 function Content() {
     return (
         <Container >
         <Row>
           <Col sm={6}>
-            One of three columns
+          <Typography variant="h8" component="h3" className='gradient__text'>
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+          </Typography>
           </Col>
           <Col sm={6}>
           <img src={ai} alt="ai" height={600}/>
